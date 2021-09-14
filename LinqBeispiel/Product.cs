40b166abb06124d0eead44ea45e6b6406d9c9fe7
JannickL <jannick.leismann@gmx.de>
2021-09-14 14:41:52 +0200

    Project commit

---
+++ LinqBeispiel/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqBeispiel
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public DateTime BestBeforeDate { get; set; }
+
+        public static List<Product> CreateProducts()
+        {
+            List<Product> list = new List<Product>()
+            {
+                new Product(){Name = "Apfel", Category = "Obst", Price = 0.45m, BestBeforeDate = Convert.ToDateTime("21.09.2021") },
+                new Product(){Name = "Katzenfutter Trocken", Category = "Tiernahrung", Price = 3.99m, BestBeforeDate = Convert.ToDateTime("01.03.2022") },
+                new Product(){Name = "Hundefutter Trocken", Category = "Tiernahrung", Price = 5.49m, BestBeforeDate = Convert.ToDateTime("22.06.2023") },
+                new Product(){Name = "Banane", Category = "Obst", Price = 0.99m, BestBeforeDate = Convert.ToDateTime("29.09.2021") },
+                new Product(){Name = "Tiefkühl Pizza", Category = "Tiefkühlware", Price = 2.39m, BestBeforeDate = Convert.ToDateTime("10.05.2022") },
+                new Product(){Name = "Tiefkühl Fischfilet", Category = "Tiefkühlware", Price = 6.99m, BestBeforeDate = Convert.ToDateTime("12.02.2022") },
+                new Product(){Name = "Tiefkühl Lasagne", Category = "Tiefkühlware", Price = 3.19m, BestBeforeDate = Convert.ToDateTime("28.07.2023") },
+                new Product(){Name = "Katzenfutter Nass", Category = "Tiernahrung", Price = 2.99m, BestBeforeDate = Convert.ToDateTime("15.01.2022") },
+                new Product(){Name = "Hundefutter Nass", Category = "Tiernahrung", Price = 3.49m, BestBeforeDate = Convert.ToDateTime("27.04.2022") },
+                new Product(){Name = "Schlangengurke", Category = "Gemüse", Price = 0.30m, BestBeforeDate = Convert.ToDateTime("10.10.2021") }
+            };
+
+            return list;
+        }
+    }
+}
---
+++ LinqBeispiel/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqBeispiel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get list with all products
+            List<Product> productList = Product.CreateProducts();
+
+            // Add some LINQ stuff here :-)
+            productList = productList
+                .Where(x => x.Category == "Tiefkühlware" || x.Category == "Obst")
+                .OrderByDescending(x => x.Price)
+                .ToList();
+
+            decimal totalPrice = productList.Sum(x => x.Price);
+
+            Console.WriteLine("Gesamtpreis: " + totalPrice + " EUR\n");
+
+            // Write products in console
+            foreach(Product product in productList)
+            {
+                string content = string.Format("{0} ({1}) | Preis: {2} EUR | Mindestens haltbar bis: {3}\n", 
+                    product.Name, product.Category, product.Price, product.BestBeforeDate.ToShortDateString());
+
+                Console.WriteLine(content);
+            }
+        }
+    } 
+}
